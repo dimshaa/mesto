b99ce68cf15a10e8e895028237ad44e540fdb750
Dmitry Shaposhnikov <ihatepost@gmail.com>
2022-04-09 10:20:04 +0300

    add initial cards render

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,61 @@
-let profile = document.querySelector('.profile');
-let profileEditBtn = profile.querySelector('.profile__edit-btn');
-let profileUsername = profile.querySelector('.profile__username');
-let profileUserbio = profile.querySelector('.profile__userbio');
+const profile = document.querySelector('.profile');
+const profileEditBtn = profile.querySelector('.profile__edit-btn');
+const profileUsername = profile.querySelector('.profile__username');
+const profileUserbio = profile.querySelector('.profile__userbio');
 
-let editWindow = document.querySelector('.popup');
-let editWindowCloseBtn = editWindow.querySelector('.popup__close-btn');
-let formElement = editWindow.querySelector('.popup__container');
-let formInputUsername = editWindow.querySelector('.popup__input_type_username');
-let formInputUserbio = editWindow.querySelector('.popup__input_type_userbio');
+const editWindow = document.querySelector('.popup');
+const editWindowCloseBtn = editWindow.querySelector('.popup__close-btn');
+const formElement = editWindow.querySelector('.popup__container');
+const formInputUsername = editWindow.querySelector('.popup__input_type_username');
+const formInputUserbio = editWindow.querySelector('.popup__input_type_userbio');
 
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsList = document.querySelector('.cards__list');
+
+
+function setInitialCards() {
+  const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+  const cardsToAdd = initialCards.map(renderCard);
+  cardsList.append(...cardsToAdd);
+}
+
+function renderCard(element) {
+   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+   const cardCaption = cardElement.querySelector('.card__caption');
+   const cardImage = cardElement.querySelector('.card__image');
+   const cardLikeBtn = cardElement.querySelector('.card__like-btn');
+
+   cardCaption.textContent = element.name;
+   cardImage.setAttribute('src', element.link);
+
+   return cardElement;
+}
 
 function openEditWindow() {
   editWindow.classList.add('popup_opened');
@@ -30,3 +77,5 @@ function formSubmitHandler(event) {
 profileEditBtn.addEventListener('click', openEditWindow);
 editWindowCloseBtn.addEventListener('click', closeEditWindow);
 formElement.addEventListener('submit', formSubmitHandler);
+
+setInitialCards();
