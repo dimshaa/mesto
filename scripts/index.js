b708ad9d381576d473d0439fe68aad3fa00a6410
Dmitry Shaposhnikov <ihatepost@gmail.com>
2022-05-26 23:04:00 +0300

    refactor index.js & move const to constats.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,136 +1,94 @@
-import { Card } from '../components/Card.js';
-import { FormValidator } from '../components/FormValidator.js';
+import Card from '../components/Card.js';
+import FormValidator from '../components/FormValidator.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import PopupWithImage from '../components/PopupWithImage.js';
 import Section from '../components/Section.js';
+import UserInfo from '../components/UserInfo.js';
+import {
+  profileEditBtn,
+  profileEditForm,
+  cardAddBtn,
+  cardAddForm,
+  initialCards,
+  validatorConfig
+ } from '../utils/constants.js';
 
-const profile = document.querySelector('.profile');
-const profileUsername = profile.querySelector('.profile__username');
-const profileUserbio = profile.querySelector('.profile__userbio');
-const profileEditBtn = profile.querySelector('.profile__edit-btn');
-const cardAddBtn = profile.querySelector('.profile__add-btn');
+const userFormValidator = new FormValidator(validatorConfig, profileEditForm);
+const cardFormValidator = new FormValidator(validatorConfig, cardAddForm);
 
-const profileEditWindow = document.querySelector('.popup_type_profile-edit');
-const formUserElement = profileEditWindow.querySelector('.popup__form');
-const formInputUsername = profileEditWindow.querySelector('.popup__input_type_username');
-const formInputUserbio = profileEditWindow.querySelector('.popup__input_type_userbio');
-
-const cardAddWindow = document.querySelector('.popup_type_card-add');
-const formCardElement = cardAddWindow.querySelector('.popup__form');
-const formInputCardName = cardAddWindow.querySelector('.popup__input_type_card-name');
-const formInputCardUrl = cardAddWindow.querySelector('.popup__input_type_card-url');
-
-const popupWindows = document.querySelectorAll('.popup');
-
-
-
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
-const validatorConfig = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__submit-btn',
-  inactiveButtonClass: 'popup__submit-btn_disabled',
-  inputErrorClass: 'popup__input_type_error'
-};
-
-const userFormValidator = new FormValidator(validatorConfig, formUserElement);
-const cardFormValidator = new FormValidator(validatorConfig, formCardElement);
+const cardViewPopup = new PopupWithImage('.popup_type_card-view');
 
 const cardsList = new Section({
   items: initialCards,
   renderer: (item) => {
-    const newCard = new Card(item, '#card-template');
+    const newCard = new Card(
+      item,
+      '#card-template',
+      () => {
+        cardViewPopup.open({
+          name: item.name,
+          link: item.link
+        });
+      });
     const newCardElement = newCard.renderCard();
 
     cardsList.addItem(newCardElement);
   }
 }, '.cards__list');
 
-export function openWindow(popupElement) {
-  popupElement.classList.add('popup_opened');
-  window.addEventListener('keydown', handleEscKey);
-}
+const cardAddPopup = new PopupWithForm({
+  formSubmitter: (formData) => {
+    const newCard = new Card({
+      name: formData.cardName,
+      link: formData.cardUrl
+    }, 
+    '#card-template',
+    () => {
+      cardViewPopup.open({
+        name: formData.cardName,
+        link: formData.cardUrl
+      });
+    });
+    const newCardElement = newCard.renderCard();
+  
+    cardsList.addItem(newCardElement);
+    cardAddPopup.close();
+  },
+  popupSelector: '.popup_type_card-add'
+});
 
-function closeWindow(popupElement) {
-  popupElement.classList.remove('popup_opened');
-  window.removeEventListener('keydown', handleEscKey);
-}
+const profileEditPopup = new PopupWithForm({
+  formSubmitter: (formData) => {
+    userInfo.setUserInfo(formData.userName, formData.userBio);
+    profileEditPopup.close()
+  },
+  popupSelector: '.popup_type_profile-edit'
+});
 
-function handleEscKey(event) {
-  if (event.key === 'Escape') {
-    const popupToClose = document.querySelector('.popup_opened');
-    closeWindow(popupToClose);
-  }
-}
+const userInfo = new UserInfo({
+  usernameSelector: '.profile__username',
+  userbioSelector: '.profile__userbio'
+})
 
-function openProfileEditForm() {
-  formInputUsername.value = profileUsername.textContent;
-  formInputUserbio.value = profileUserbio.textContent;
+profileEditBtn.addEventListener('click', () => {
+  const currentProfile = userInfo.getUserInfo();
+
+  profileEditPopup._form.userName.value = currentProfile.username;
+  profileEditPopup._form.userBio.value = currentProfile.userbio
   userFormValidator.resetValidation();
-  openWindow(profileEditWindow);
-}
+  profileEditPopup.open();
+});
 
-function openCardAddForm() {
-  formCardElement.reset();
+cardAddBtn.addEventListener('click', () => {
   cardFormValidator.resetValidation();
-  openWindow(cardAddWindow);
-}
-
-function handleProfileFormSubmit(event) {
-  event.preventDefault();
-  profileUsername.textContent = formInputUsername.value;
-  profileUserbio.textContent = formInputUserbio.value;
-  closeWindow(profileEditWindow);
-}
-
-function handleCardFormSubmit(event) {
-  event.preventDefault();
-  const newCard = new Card({
-    name: formInputCardName.value,
-    link: formInputCardUrl.value
-  }, '#card-template');
-  const newCardElement = newCard.renderCard();
-
-  cardsList.addItem(newCardElement);
-  closeWindow(cardAddWindow);
-}
-
-profileEditBtn.addEventListener('click', openProfileEditForm);
-cardAddBtn.addEventListener('click', openCardAddForm);
-formUserElement.addEventListener('submit', handleProfileFormSubmit);
-formCardElement.addEventListener('submit', handleCardFormSubmit);
-popupWindows.forEach((popup) => {
-  popup.addEventListener('click', (event) => {
-    if (event.target.classList.contains('popup__close-btn') || (event.target === event.currentTarget)) {
-      closeWindow(popup);
-    }
-  });
+  cardAddPopup.open();
 });
 
 userFormValidator.enableValidation();
 cardFormValidator.enableValidation();
+
 cardsList.renderItems();
+
+cardViewPopup.setEventListeners();
+cardAddPopup.setEventListeners();
+profileEditPopup.setEventListeners();
---
+++ utils/constants.js
@@ -0,0 +1,44 @@
+const profile = document.querySelector('.profile');
+export const profileEditBtn = profile.querySelector('.profile__edit-btn');
+export const cardAddBtn = profile.querySelector('.profile__add-btn');
+
+const profileEditWindow = document.querySelector('.popup_type_profile-edit');
+export const profileEditForm = profileEditWindow.querySelector('.popup__form');
+
+const cardAddWindow = document.querySelector('.popup_type_card-add');
+export const cardAddForm = cardAddWindow.querySelector('.popup__form');
+
+export const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+export const validatorConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__submit-btn',
+  inactiveButtonClass: 'popup__submit-btn_disabled',
+  inputErrorClass: 'popup__input_type_error'
+};
